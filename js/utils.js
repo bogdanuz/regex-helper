bfe25931c497414360ec8fb0c2eddca0f78139b2
bd_uz <bogdandugalev@gmail.com>
2026-02-09 02:33:02 +0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -311,16 +311,23 @@ function pluralize(number, forms) {
     const n = Math.abs(number) % 100;
     const n1 = n % 10;
     
-    if (n > 10 && n < 20) {
-        return forms[2]; // триггеров
+    // 11-19: триггеров
+    if (n >= 11 && n <= 19) {
+        return forms[2];
     }
-    if (n1 > 1 && n1 < 5) {
-        return forms[1]; // триггера
-    }
+    
+    // 1, 21, 31...: триггер
     if (n1 === 1) {
-        return forms[0]; // триггер
+        return forms[0];
     }
-    return forms[2]; // триггеров
+    
+    // 2-4, 22-24, 32-34...: триггера
+    if (n1 >= 2 && n1 <= 4) {
+        return forms[1];
+    }
+    
+    // 0, 5-20, 25-30...: триггеров
+    return forms[2];
 }
 
 /**
